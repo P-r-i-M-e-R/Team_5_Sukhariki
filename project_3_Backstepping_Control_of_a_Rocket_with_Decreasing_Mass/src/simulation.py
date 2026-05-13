e03c43288cbd72d9cdf90466d2063ef12edf7cc0
P-r-i-M-e-R <138395245+P-r-i-M-e-R@users.noreply.github.com>
2026-05-13 18:11:34 +0300

    Small fixes

--- project_3_Backstepping_Control_of_a_Rocket_with_Decreasing_Mass/src/simulation.py
+++ project_3_Backstepping_Control_of_a_Rocket_with_Decreasing_Mass/src/simulation.py
@@ -21,48 +21,44 @@ class Simulation:
         # Backstepping trajectory
         bs_state = np.zeros((N, 2))
         bs_state[0] = [p["z0"], p["zdot0"]]
-        bs_T     = np.zeros(N)
-        bs_e     = np.zeros(N)
-        bs_v_e   = np.zeros(N)
+        bs_T = np.zeros(N)
+        bs_e = np.zeros(N)
+        bs_v_e = np.zeros(N)
         bs_delta = np.zeros(N)
-        bs_L1    = np.zeros(N)
-        bs_dL1   = np.zeros(N)
+        bs_L1 = np.zeros(N)
+        bs_dL1 = np.zeros(N)
 
         # PD trajectory
-        pd_state  = np.zeros((N, 2))
+        pd_state = np.zeros((N, 2))
         pd_state[0] = [p["z0"], p["zdot0"]]
-        pd_T     = np.zeros(N)
-        pd_e     = np.zeros(N)
-        pd_v_e   = np.zeros(N)
-        pd_L_PD  = np.zeros(N)
+        pd_T = np.zeros(N)
+        pd_e = np.zeros(N)
+        pd_v_e = np.zeros(N)
+        pd_L_PD = np.zeros(N)
         pd_dL_PD = np.zeros(N)
 
         for i, t in enumerate(t_arr):
             m = self.system.mass(t)
             mu = self.system.m0 / m
-            zd, zd_dot, zd_ddot = self.system.reference(
-                t, p["z_step"], p["t_step"]
-            )
+            zd, zd_dot, zd_ddot = self.system.reference(t, p["z_step"], p["t_step"])
 
-            # --- backstepping ---
+            # Backstepping
             z_bs, zdot_bs = bs_state[i]
-            T_bs, e_bs, ve_bs, delta_bs = self.bs.compute(
-                z_bs, zdot_bs, zd, zd_dot, zd_ddot, m
-            )
-            bs_T[i]     = T_bs
-            bs_e[i]     = e_bs
-            bs_v_e[i]   = ve_bs
+            T_bs, e_bs, ve_bs, delta_bs = self.bs.compute(z_bs, zdot_bs, zd, zd_dot, zd_ddot, m)
+            bs_T[i] = T_bs
+            bs_e[i] = e_bs
+            bs_v_e[i] = ve_bs
             bs_delta[i] = delta_bs
-            bs_L1[i]    = self.bs.lyapunov(e_bs, delta_bs)
-            bs_dL1[i]   = self.bs.lyapunov_dot(e_bs, delta_bs)
+            bs_L1[i] = self.bs.lyapunov(e_bs, delta_bs)
+            bs_dL1[i] = self.bs.lyapunov_dot(e_bs, delta_bs)
 
-            # --- PD ---
+            # PD
             z_pd, zdot_pd = pd_state[i]
             T_pd, e_pd, ve_pd = self.pd.compute(z_pd, zdot_pd, zd, zd_dot)
-            pd_T[i]     = T_pd
-            pd_e[i]     = e_pd
-            pd_v_e[i]   = ve_pd
-            pd_L_PD[i]  = self.pd.lyapunov_pd(e_pd, ve_pd)
+            pd_T[i] = T_pd
+            pd_e[i] = e_pd
+            pd_v_e[i] = ve_pd
+            pd_L_PD[i] = self.pd.lyapunov_pd(e_pd, ve_pd)
             pd_dL_PD[i] = self.pd.lyapunov_pd_dot(e_pd, ve_pd, mu)
 
             if i < N - 1:
@@ -72,9 +68,7 @@ class Simulation:
                 dxdt_pd = self.system.dynamics(pd_state[i], T_pd, t)
                 pd_state[i + 1] = pd_state[i] + p["dt"] * dxdt_pd
 
-        zd_arr = np.array(
-            [self.system.reference(t, p["z_step"], p["t_step"])[0] for t in t_arr]
-        )
+        zd_arr = np.array([self.system.reference(t, p["z_step"], p["t_step"])[0] for t in t_arr])
 
         return {
             "t": t_arr,
